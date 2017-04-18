a8e69fa6d7f9539dd06d30ac74ec9ff2291edd36
Maromaro sasaki <sasaki@maromaro.jp>
2017-04-18 10:41:52 +0900

    とりあえずアップ

---
+++ js/jquery.img-bottom.js
@@ -0,0 +1,53 @@
+(function ($) {
+
+  $.fn.imgBottom = function () {
+    $target = $(this);
+    $(window).on('resize', function () {
+      $imgbottomspan = $parent.find('.img-bottom-span');
+      if ($imgbottomspan.length > 0) {
+        $imgbottomspan.remove();
+      }
+
+      dofunc($target);
+    });
+    dofunc($target);
+  };
+  function dofunc($target) {
+    $target.each(function () {
+      $img = $(this);
+      $parent = $(this).parent();
+      $imgbottomspan = $parent.find('.img-bottom-span');
+
+      cnt = 1;
+      var decrease = 0;
+      if ($parent.height() <= $img.outerHeight()) {
+        return;
+      }
+      while (cnt <= 50 && ( $imgbottomspan.length < 1 || $parent.outerHeight() <= $img.outerHeight() + $imgbottomspan.height() )) {
+        aa();
+        $imgbottomspan = $parent.find('.img-bottom-span');
+        cnt++;
+        console.log(1);
+        decrease += 1;
+      }
+
+      function aa() {
+        if ($imgbottomspan.length > 0) {
+          $imgbottomspan.remove();
+        }
+        img_height = $img.outerHeight();
+        $parent_clone = $parent.clone(true);
+        $parent_clone.hide();
+        $parent.after($parent_clone);
+        parent_height = $parent_clone.outerHeight();
+        $insert = $('<span class="img-bottom-span">');
+
+
+        $insert.css({'float': 'right', 'display': 'inline-block', 'width': '1px'});
+        $insert.height(parent_height - img_height - decrease);
+        $img.before($insert);
+      }
+
+    });
+  }
+})(jQuery);
