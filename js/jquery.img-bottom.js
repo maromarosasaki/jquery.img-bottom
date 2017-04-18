e2d6ed09e574587f6a15a113c467e47bc42b4d69
Maromaro sasaki <sasaki@maromaro.jp>
2017-04-18 14:36:49 +0900

    offsetを設定できるようにしました。

--- js/jquery.img-bottom.js
+++ js/jquery.img-bottom.js
@@ -1,6 +1,9 @@
 (function ($) {
 
-  $.fn.imgBottom = function () {
+  $.fn.imgBottom = function (offset) {
+    if( !offset ){
+      offset = 0;
+    }
     $target = $(this);
     $(window).on('resize', function () {
       $imgbottomspan = $parent.find('.img-bottom-span');
@@ -11,43 +14,46 @@
       dofunc($target);
     });
     dofunc($target);
-  };
-  function dofunc($target) {
-    $target.each(function () {
-      $img = $(this);
-      $parent = $(this).parent();
-      $imgbottomspan = $parent.find('.img-bottom-span');
 
-      cnt = 1;
-      var decrease = 0;
-      if ($parent.height() <= $img.outerHeight()) {
-        return;
-      }
-      while (cnt <= 50 && ( $imgbottomspan.length < 1 || $parent.outerHeight() <= $img.outerHeight() + $imgbottomspan.height() )) {
-        aa();
+
+    function dofunc($target) {
+      $target.each(function () {
+        $img = $(this);
+        img_height = $img.outerHeight() -offset;
+        $parent = $(this).parent();
         $imgbottomspan = $parent.find('.img-bottom-span');
-        cnt++;
-        console.log(1);
-        decrease += 1;
-      }
 
-      function aa() {
-        if ($imgbottomspan.length > 0) {
-          $imgbottomspan.remove();
+        cnt = 1;
+        var decrease = 0;
+        if ($parent.height() <= $img.outerHeight()) {
+          return;
         }
-        img_height = $img.outerHeight();
-        $parent_clone = $parent.clone(true);
-        $parent_clone.hide();
-        $parent.after($parent_clone);
-        parent_height = $parent_clone.outerHeight();
-        $insert = $('<span class="img-bottom-span">');
+        while (cnt <= 50 && ( $imgbottomspan.length < 1 || $parent.outerHeight() <= img_height + $imgbottomspan.height() )) {
+          aa();
+          $imgbottomspan = $parent.find('.img-bottom-span');
+          cnt++;
+          console.log(1);
+          decrease += 1;
+        }
+
+        function aa() {
+          if ($imgbottomspan.length > 0) {
+            $imgbottomspan.remove();
+          }
+          $parent_clone = $parent.clone(true);
+          $parent_clone.hide();
+          $parent.after($parent_clone);
+          parent_height = $parent_clone.outerHeight();
+          $insert = $('<span class="img-bottom-span">');
 
 
-        $insert.css({'float': 'right', 'display': 'inline-block', 'width': '1px'});
-        $insert.height(parent_height - img_height - decrease);
-        $img.before($insert);
-      }
+          $insert.css({'float': 'right', 'display': 'inline-block', 'width': '1px'});
+          $insert.height(parent_height - img_height - decrease);
+          $img.before($insert);
+        }
 
-    });
-  }
+      });
+    }
+
+  };
 })(jQuery);
